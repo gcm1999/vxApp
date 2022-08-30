727d184a19ccdd8827eceb7752faae220468ab1a
gcm <1453400328@qq.com>
2022-08-30 09:59:07 +0800

    测试

---
+++ pages/test/test.js
@@ -0,0 +1,96 @@
+// pages/test/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    num: 0,
+    msg: "",
+    flag: true,
+    arr:[{name:"gg",id:1},{name:"cc",id:2},{name:"mm",id:3}]
+
+  },
+  changeFlag() {
+    this.setData({
+      flag: !this.data.flag
+    })
+  },
+  upNum() {
+    this.setData({
+      num: ++this.data.num,
+    })
+  },
+  upNum2(e) {
+    // console.log(e.target.dataset);
+    this.setData({
+      num: e.target.dataset.num + this.data.num
+    })
+  },
+  inputHandler(e) {
+    // console.log(e);
+    this.setData({
+      msg: e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  // 测试事件
+  testEvent(event) {
+    console.log(event);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
